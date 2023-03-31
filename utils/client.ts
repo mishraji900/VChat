7e191f83072fa6b761a3dc3f44d905c0befe764e
mishraji900 <117895206+mishraji900@users.noreply.github.com>
2023-03-31 23:47:59 +0530

    ...

--- utils/client.ts
+++ utils/client.ts
@@ -2,6 +2,8 @@ import {createClient} from '@sanity/client';
 
 export const client = createClient({
   projectId: 'w4tqq5bz',
+
+  
   dataset: 'production',
   apiVersion: '2023-03-30',
   useCdn: false,
